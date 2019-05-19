9922b08dec68cc5d69101ce25cd745e3d8e68273
Ubuntu <ubuntu@ip-172-31-21-249.ap-northeast-2.compute.internal>
2019-05-19 03:20:16 +0000

    alert needed

--- my-app/models/todo.js
+++ my-app/models/todo.js
@@ -4,8 +4,8 @@ var todoSchema = mongoose.Schema({
  title: {type: String},
  content: {type: String},
  dueDate: {type: Date},
- priority: {type: Number},
- done: {type: Boolean}
+ priority: {type: String},
+ done: {type: String}
 });
 
 var Todo = mongoose.model('todo', todoSchema);
--- my-app/routes/index.js
+++ my-app/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 var mongoose = require('mongoose');
-//var Promise = require('promise');
+var Promise = require('promise');
 var Todo = require('../models/todo');
 
 /* GET home page. */
@@ -10,60 +10,73 @@ router.get('/', function(req, res, next) {
   Todo.find({}, function(err, todos) {
     if (err) return res.render('error', {message:err.message, error:{}});
     var result = [];
-    /*
+    var expired = [];
     todos.forEach((todo) => {
       var now = new Date();
       var td = todo.toObject();
-      td.expired = false;
-      if (now.getTime() - td.dueDate.getTime() <= 0)
-        td.expired = true;
-      result.push(td);
-    });*/
-    res.render('index', todos);
-  });
-});
-/*
-router.get('/', function(req, res, next) {
-  Todo.find({}, function(err, todos) {
-    if (err) return res.render('error', {message: err.message,error: {}});
-    var result = [];
-    todos.forEach((todo) => {
-      var now = new Date();
-      var td = todo.toObject();
-      td.expired = false;
-      if (now.getTime() - td.dueDate.getTime() <= 0)
-        td.expired = true;
+      if (td.dueDate!=undefined && now.getTime() - td.dueDate.getTime() >= 0)
+        expired.push(td);
+      td.date = formatDate(td.dueDate);
       result.push(td);
     });
-    res.render('index', result);
-  }
-});*/
-/*
+    res.render('index', {result:result, expired:expired});
+  });
+});
+
+function formatDate(date) {
+  var d = new Date(date),
+  month = "" + (d.getMonth() + 1),
+  day = "" + d.getDate(),
+  year = d.getFullYear();
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+  return [year, month, day].join("-");
+}
+
 router.post('/todo', function(req, res, next) {
   var todo = new Todo({
     'title':req.body.title,
     'content':req.body.content,
     'priority':req.body.priority,
-    'done':req.body.done});
+    'done':"0"
+  });
+
   todo.save(async function(err, todo) {
-    if (req.body.dueDate != undefined)
+    if (req.body.dueDate != undefined) {
+      console.log(req.body.dueDate);
       await Todo.findOneAndUpdate({'_id':todo._id}, {$addToSet:{'dueDate':req.body.dueDate}});
-    return res.json({'todo':todo});
+    }
+    return res.json({'message':'post success'});
   });
 });
 
-router.put('/todo', async function(req, res, next) {
-  await Todo.findOneAndUpdate(
-    {'_id':req.body.todoId},
-    {$set:{'title':req.body.title,
-           'content':req.body.content,
-           'priority':req.body.priority,
-           'done':req.body.done}});
-  return res.json({'message':'modify success'});
+router.put('/todo', function(req, res, next) {
+  if (req.body.dueDate != undefined) {
+    Todo.findOneAndUpdate(
+      {'_id':req.body.todoId},
+      {$set:{'title':req.body.title,
+             'content':req.body.content,
+             'priority':req.body.priority,
+             'done':req.body.done,
+             'dueDate':req.body.dueDate}},
+      function(err, todo) {
+        if (err) return res.render('error', {message:err.message, error:{}});
+        return res.json({'message':'modify success'});
+    });
+  }
+  else {
+    Todo.findOneAndUpdate(
+      {'_id':req.body.todoId},
+      {$set:{'title':req.body.title, 'content':req.body.content, 'priority':req.body.priority, 'done':req.body.done}},
+      function(err, todo) {
+        if (err) return res.render('error', {message:err.message, error:{}});
+        return res.json({'message':'modify success'});
+    });
+  }
 });
 
 router.delete('/todo', async function(req, res, next) {
   await Todo.deleteOne({'_id':req.body.todoId});
   return res.json({'message':'delete success'});
-});*/
+});
 module.exports = router;
